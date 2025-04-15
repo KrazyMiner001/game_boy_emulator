44837eba09a3b3c702b7e230626b86b320e23c2e
KrazyMiner001 <krazyminer001@outlook.com>
2025-04-15 16:41:45 +0200

    feat(cpu): :sparkles: Implemented registers

---
+++ src/cpu/registers.cpp
@@ -0,0 +1,140 @@
+#include "registers.hpp"
+#include <stdlib.h>
+
+namespace cpu {
+    Registers::Registers() {
+        data = (uint8_t *) malloc(12);
+    }
+
+    Registers::~Registers() {
+        delete data;
+    }
+
+    uint8_t Registers::get_a() {
+        return data[0];
+    }
+
+    uint8_t Registers::get_f() {
+        return data[1];
+    }
+
+    uint8_t Registers::get_b() {
+        return data[2];
+    }
+
+    uint8_t Registers::get_c() {
+        return data[3];
+    }
+
+    uint8_t Registers::get_d() {
+        return data[4];
+    }
+
+    uint8_t Registers::get_e() {
+        return data[5];
+    }
+
+    uint8_t Registers::get_h() {
+        return data[6];
+    }
+
+    uint8_t Registers::get_l() {
+        return data[7];
+    }
+
+    uint8_t Registers::get_r8(Register_8bit r8) {
+        return data[(ptrdiff_t) r8];
+    }
+
+    uint16_t Registers::get_af() {
+        return ((uint16_t *) data)[0];
+    }
+
+    uint16_t Registers::get_bc() {
+        return ((uint16_t *) data)[1];
+    }
+
+    uint16_t Registers::get_de() {
+        return ((uint16_t *) data)[2];
+    }
+
+    uint16_t Registers::get_hl() {
+        return ((uint16_t *) data)[3];
+    }
+
+    uint16_t Registers::get_sp() {
+        return ((uint16_t *) data)[4];
+    }
+
+    uint16_t Registers::get_pc() {
+        return ((uint16_t *) data)[5];
+    }
+
+    uint16_t Registers::get_r16(Register_16bit r16) {
+        return ((uint16_t *) data)[(ptrdiff_t) r16];
+    }
+
+    void Registers::set_a(uint8_t value) {
+        data[0] = value;
+    }
+
+    void Registers::set_f(uint8_t value) {
+        data[1] = value;
+    }
+
+    void Registers::set_b(uint8_t value) {
+        data[2] = value;
+    }
+
+    void Registers::set_c(uint8_t value) {
+        data[3] = value;
+    }
+
+    void Registers::set_d(uint8_t value) {
+        data[4] = value;
+    }
+
+    void Registers::set_e(uint8_t value) {
+        data[5] = value;
+    }
+
+    void Registers::set_h(uint8_t value) {
+        data[6] = value;
+    }
+
+    void Registers::set_l(uint8_t value) {
+        data[7] = value;
+    }
+
+    void Registers::set_r8(Register_8bit r8, uint8_t value) {
+        data[(ptrdiff_t) r8] = value;
+    }
+
+    void Registers::set_af(uint16_t value) {
+        data[0] = value;
+    }
+
+    void Registers::set_bc(uint16_t value) {
+        data[1] = value;
+    }
+
+    void Registers::set_de(uint16_t value) {
+        data[2] = value;
+    }
+
+    void Registers::set_hl(uint16_t value) {
+        data[3] = value;
+    }
+
+    void Registers::set_sp(uint16_t value) {
+        data[4] = value;
+    }
+
+    void Registers::set_pc(uint16_t value) {
+        data[5] = value;
+    }
+
+    void Registers::set_r16(Register_16bit r16, uint16_t value) {
+        data[(ptrdiff_t) r16] = value;
+    }
+}
--- src/cpu/registers.hpp
+++ src/cpu/registers.hpp
@@ -3,7 +3,7 @@
 
 namespace cpu {
     enum class Register_8bit {
-        A, B, C, D, E, H, L, Flags
+        A, F, B, C, D, E, H, L
     };
 
     enum class Register_16bit {
@@ -11,32 +11,53 @@ namespace cpu {
     };
 
     class Registers {
-    private:
-        uint8_t data[12];
+        private:
+            uint8_t* data;
 
-    public:
-        Registers();
+        public:
+            Registers();
+            ~Registers();
 
-        uint8_t get_a();
-        uint8_t get_b();
-        uint8_t get_c();
-        uint8_t get_d();
-        uint8_t get_e();
-        uint8_t get_flags();
-        
-        uint8_t get_h();
-        uint8_t get_l();
+            uint8_t get_a();
+            uint8_t get_f();
+            uint8_t get_b();
+            uint8_t get_c();
+            uint8_t get_d();
+            uint8_t get_e();
+            uint8_t get_h();
+            uint8_t get_l();
 
-        uint8_t get_r8(Register_8bit r8);
-        
-        uint16_t get_af();
-        uint16_t get_bc();
-        uint16_t get_de();
-        uint16_t get_hl();
-        
-        uint16_t get_sp();
-        uint16_t get_pc();
+            uint8_t get_r8(Register_8bit r8);
+            
+            uint16_t get_af();
+            uint16_t get_bc();
+            uint16_t get_de();
+            uint16_t get_hl();
+            
+            uint16_t get_sp();
+            uint16_t get_pc();
 
-        uint16_t get_r16(Register_16bit r16);
+            uint16_t get_r16(Register_16bit r16);
+
+            void set_a(uint8_t value);
+            void set_f(uint8_t value);
+            void set_b(uint8_t value);
+            void set_c(uint8_t value);
+            void set_d(uint8_t value);
+            void set_e(uint8_t value);
+            void set_h(uint8_t value);
+            void set_l(uint8_t value);
+
+            void set_r8(Register_8bit r8, uint8_t value);
+
+            void set_af(uint16_t value);
+            void set_bc(uint16_t value);
+            void set_de(uint16_t value);
+            void set_hl(uint16_t value);
+
+            void set_sp(uint16_t value);
+            void set_pc(uint16_t value);
+
+            void set_r16(Register_16bit r16, uint16_t value);
     };
 }
